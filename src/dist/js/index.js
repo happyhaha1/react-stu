e38ecfa089f05c51e1b151e400ed84508e08f6d4
happyhaha1 <474248257@qq.com>
2016-09-07 13:25:03 +0800

    玩css

--- src/dist/js/index.js
+++ src/dist/js/index.js
@@ -4,15 +4,27 @@
  */
 import React from "react";
 import ReactDOM, { render } from "react-dom";
+import style from "../css/index.css";
+import bg from "../img/bg.jpg";
+import bg2 from "../img/bg2.jpg";
+
 
 class Hello extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    click() {
+        $("."+style.photo).attr("src",bg2);
+        console.log(style.photo);
+    }
+
     render() {
         return (
-            <div>Hello,World!!!!</div>
+            <div className="MyComponent-wrapper">
+                <h1>Hello world</h1>
+                <img className={style.photo} onClick={this.click} src={bg}/>
+            </div>
         );
     }
 }
